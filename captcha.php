f6469afa88fc1c1f3ab899e7c6363292f163f737
Thoriq <155645840+thoriqnst0610@users.noreply.github.com>
2025-02-24 12:09:00 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,12 +1,23 @@
 <?php
 session_start();
-require 'vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
+
+// use Gregwar\Captcha\CaptchaBuilder;
+
+// $builder = new CaptchaBuilder;
+// $builder->build();
+
+// $_SESSION['captcha'] = strtolower($builder->getPhrase());
+
+// header('Content-type: image/jpeg');
+// $builder->output();
 
 use Gregwar\Captcha\CaptchaBuilder;
 
 $builder = new CaptchaBuilder;
 $builder->build();
-$_SESSION['phrase'] = $builder->getPhrase();
+
+$_SESSION['captcha'] = strtolower($builder->getPhrase());
 
 header('Content-type: image/jpeg');
 $builder->output();
